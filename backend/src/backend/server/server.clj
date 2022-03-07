07ee68fd4390668eb20e007f4fce4a8b78495bd5
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-07 23:56:33 +0200

    Add server restart functionality for dev in REPL

--- backend/src/backend/server/server.clj
+++ backend/src/backend/server/server.clj
@@ -2,9 +2,24 @@
   (:require [backend.server.handler :as handler]
             [ring.adapter.jetty :as jetty]))
 
-(defn start []
-  (jetty/run-jetty handler/app {:port 3000, :join? false})
-  (println "server running in port 3000"))
+(defonce running-server (atom nil))
+
+(defn start
+  []
+  (when (nil? @running-server)
+    (clojure.core/reset! running-server (jetty/run-jetty #'handler/app {:port 3000, :join? false}))
+    (println "server running in port 3000")))
+
+(defn stop
+  []
+  (when-not (nil? @running-server)
+    (.stop @running-server)
+    (clojure.core/reset! running-server nil)))
+
+(defn restart
+  []
+  (stop)
+  (start))
 
 (defn -main [& args]
   (start))
