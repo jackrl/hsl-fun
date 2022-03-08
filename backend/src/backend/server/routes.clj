11d9d2640125f243920091cd35e3cf8caf0e0ae0
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 01:26:48 +0200

    Create route /api/stops

--- backend/src/backend/server/routes.clj
+++ backend/src/backend/server/routes.clj
@@ -1,9 +1,17 @@
-(ns backend.server.routes)
+(ns backend.server.routes
+  (:require [backend.api.hsl :as hsl-api]
+            [backend.model.stop :as stop-model]
+            [clojure.spec.alpha :as s]))
 
 (def routes
-  ["/hello-world"
-   {:get {:summary    "Hello World!"
-          :responses  {200 {:body {:hello string?}}}
-          :handler    (fn [_]
-                        {:status 200
-                         :body   {:hello "world"}})}}])
+  ["/api"
+   ["/stops"
+    {:post {:summary    "Get stops at lat and lon within radius"
+            :parameters {:body {:lat    float?
+                                :lon    float?
+                                :radius int?}}
+            :responses  {200 {:body {:stops (s/coll-of stop-model/stop-spec)}}}
+            :handler    (fn [{{{:keys [lat lon radius]} :body} :parameters}]
+                          (let [hsl-nodes (hsl-api/get-hsl-nodes-in-radius lat lon radius)]
+                            {:status 200
+                             :body   {:stops (map stop-model/hsl-node->stop hsl-nodes)}}))}}]])
