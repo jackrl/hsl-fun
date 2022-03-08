a012e56fc2ea7ce373f774d7a0353baf30d29a11
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 01:26:26 +0200

    Add stop-spec

--- backend/src/backend/model/stop.clj
+++ backend/src/backend/model/stop.clj
@@ -1,4 +1,13 @@
-(ns backend.model.stop)
+(ns backend.model.stop
+  (:require [spec-tools.data-spec :as ds]))
+
+(def stop-spec
+  (ds/spec
+    {:name       string?
+     :lat        float?
+     :lon        float?
+     :distance   int?
+     :accessible boolean?}))
 
 (defn hsl-node->stop
   [{:keys [distance stop]}]
