11172946b185c383bc1117b1d5aafdab2075f492
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 00:24:15 +0200

    Add get-hsl-nodes-in-radius api call

--- backend/src/backend/api/hsl.clj
+++ backend/src/backend/api/hsl.clj
@@ -1,16 +1,29 @@
 (ns backend.api.hsl
-  (:require [clj-http.client :as client]
-            [graphql-query.core :refer [graphql-query]]))
+  (:require [clj-http.client :as client]))
 
 (def url "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql")
 
-(defn get-stop-by-id
-  []
-  (let [query    (graphql-query {:queries [[:stop {:id "HSL:1173434"} [:name :lat :lon]]]})
+(defn get-hsl-nodes-in-radius
+  [lat lon radius]
+  (let [query    (str "{stopsByRadius (lat: " lat ", lon: " lon ", radius: " radius ") {
+                          edges {
+                            node {
+                              distance
+                                stop {
+                                  name
+                                  lat
+                                  lon
+                                  wheelchairBoarding
+                                }
+                              }
+                            }
+                          }
+                       }")
         response (client/post url {:content-type "application/graphql"
                                    :body         query
                                    :as           :json})]
     (-> response
         :body
         :data
-        :stop)))
+        :stopsByRadius
+        :edges)))
