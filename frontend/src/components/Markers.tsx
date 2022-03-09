5bdc7b5b6becbfd2694971aaeddf947bde0f9272
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 19:13:25 +0200

    Add the creation of markers based on the backend response

--- frontend/src/components/Markers.tsx
+++ frontend/src/components/Markers.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import { Circle, Marker, Popup, useMap, useMapEvents } from "react-leaflet";
+import { Stop } from '../types/stop';
+import { getStopsInRadius } from '../api/hsl';
 
 export function Markers() {
     const map = useMap()
     const [position, setPosition] = useState(map.getCenter())
     const [radius, setRadius] = useState(500)
+    const [stops, setStops] = useState<Array<Stop>>([])
+
+    useEffect(() => {
+        async function getStops() {
+            const stops = await getStopsInRadius(position.lat, position.lng, radius)
+            setStops(stops)
+            console.log(stops)
+        }
+
+        getStops()
+    }, [position, radius])
 
     useMapEvents({
         click(e) {
             setPosition(e.latlng)
             map.flyTo(e.latlng, map.getZoom())
+            console.log('pos',e.latlng)
         },
     })
 
-    const icon = L.icon({ iconUrl: "/images/marker-icon.png", shadowUrl: "/images/marker-shadow.png" });
+    const markerIcon = L.icon({ iconUrl: "/images/marker-icon.png", shadowUrl: "/images/marker-shadow.png" });
 
     return (
         <div>
@@ -23,6 +37,16 @@ export function Markers() {
                 radius={radius}
                 stroke={false}
             />
+
+            {stops.map((stop, i) => {
+                return (<Marker position={L.latLng(stop.lat, stop.lon)} icon={markerIcon}>
+                    <Popup>
+                        <b>{stop.name}</b><br/>
+                        Distance:   {stop.distance}m<br/>
+                        Accessible: {stop.accessible ? "Yes" : "No"}
+                    </Popup>
+                  </Marker>)
+            })}
         </div>
     )
 }
