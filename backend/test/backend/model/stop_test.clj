a4ce4090ed3f0c231a3be477cf1b5b98e1ae0444
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 18:21:37 +0200

    Refactor clojure.data import name

--- backend/test/backend/model/stop_test.clj
+++ backend/test/backend/model/stop_test.clj
@@ -1,7 +1,7 @@
 (ns backend.model.stop_test
   (:require [clojure.test :refer :all]
             [backend.model.stop :as stop-model]
-            [clojure.data :as cd]))
+            [clojure.data :as data]))
 
 (deftest hsl-node->stop
   (testing "Transformation is done correctly when :wheelChairBoarding is 'POSSIBLE'"
@@ -10,7 +10,7 @@
                                                         :lat                1.111111
                                                         :lon                2.222222
                                                         :wheelchairBoarding "POSSIBLE"}})]
-      (is (= [nil nil result] (cd/diff result {:name       "NAME_X"
+      (is (= [nil nil result] (data/diff result {:name       "NAME_X"
                                                :lat        1.111111
                                                :lon        2.222222
                                                :distance   333,
@@ -21,7 +21,7 @@
                                                         :lat                2.222222
                                                         :lon                1.111111
                                                         :wheelchairBoarding "NOT_POSSIBLE"}})]
-      (is (= [nil nil result] (cd/diff result {:name       "NAME_Y"
+      (is (= [nil nil result] (data/diff result {:name       "NAME_Y"
                                                :lat        2.222222
                                                :lon        1.111111
                                                :distance   444,
