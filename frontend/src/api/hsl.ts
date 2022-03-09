c47e2eed80eb214bc7ec2908a81ec781f1b62ba0
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 19:32:27 +0200

    Implement getStopsInRadius

--- frontend/src/api/hsl.ts
+++ frontend/src/api/hsl.ts
@@ -1,32 +1,9 @@
+import axios from "axios"
 import { Stop } from "../types/stop"
 
 
-export async function getStopsInRadius(lat: number, lng: number, radius: number): Promise<Array<Stop>> {
-    const stopsResponse = {
-        "stops": [
-            {
-                "name": "Pasilan asema",
-                "lat": 60.1981,
-                "lon": 24.93365,
-                "distance": 225,
-                "accessible": true
-            },
-            {
-                "name": "Pasilan asema",
-                "lat": 60.198075,
-                "lon": 24.933694,
-                "distance": 261,
-                "accessible": false
-            },
-            {
-                "name": "Pasilan asema",
-                "lat": 60.198002,
-                "lon": 24.933908,
-                "distance": 294,
-                "accessible": false
-            }
-        ]
-    }
+export async function getStopsInRadius(lat: number, lon: number, radius: number): Promise<Array<Stop>> {
+    const response = await axios.post<{stops: Array<Stop>}>("http://localhost:3000/api/stops", {lat, lon, radius})
 
-    return stopsResponse.stops
+    return response.data.stops
 }
