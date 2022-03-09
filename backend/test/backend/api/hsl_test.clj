5ccdd414b1581481419996eaa5a8639eba90247f
Jacinto Ramirez Lahti <jackrl@users.noreply.github.com>
2022-03-09 18:23:12 +0200

    Reformat hsl_test file

--- backend/test/backend/api/hsl_test.clj
+++ backend/test/backend/api/hsl_test.clj
@@ -25,9 +25,9 @@
             lon    24.93129
             radius 300
             result (hsl-api/get-hsl-nodes-in-radius lat lon radius)]
-        (is (= [nil nil result] (data/diff result  [{:distance 225, :stop {:name "Pasilan asema", :lat 60.1981, :lon 24.93365, :wheelchairBoarding "POSSIBLE"}}
-                                                       {:distance 261, :stop {:name "Pasilan asema", :lat 60.198075, :lon 24.933694, :wheelchairBoarding "NO_INFORMATION"}}
-                                                       {:distance 294, :stop {:name "Pasilan asema", :lat 60.198002, :lon 24.933908, :wheelchairBoarding "NO_INFORMATION"}}]))))))
+        (is (= [nil nil result] (data/diff result [{:distance 225, :stop {:name "Pasilan asema", :lat 60.1981, :lon 24.93365, :wheelchairBoarding "POSSIBLE"}}
+                                                   {:distance 261, :stop {:name "Pasilan asema", :lat 60.198075, :lon 24.933694, :wheelchairBoarding "NO_INFORMATION"}}
+                                                   {:distance 294, :stop {:name "Pasilan asema", :lat 60.198002, :lon 24.933908, :wheelchairBoarding "NO_INFORMATION"}}]))))))
 
   (testing "Returns one hsl nodes correctly when hsl api returns just one node"
     (with-redefs [clj-http.client/post (constantly {:status 200
@@ -51,5 +51,4 @@
             radius 100
             result (hsl-api/get-hsl-nodes-in-radius lat lon radius)]
         (is (empty? result)))
-      ))
-  )
+      )))
